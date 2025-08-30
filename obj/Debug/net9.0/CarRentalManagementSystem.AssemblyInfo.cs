001478697c8235771da7430c82359061e43c9dfd
MD.Shadhin <shadhin.cse71@gmail.com>
2025-08-30 15:13:05 +0600

    update and modify the dashboard in fleet management

--- obj/Debug/net9.0/CarRentalManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/CarRentalManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f305ed686225d509ba6fe8ff15e5da1aad1d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56834e82282b019416276409b46f34f53369d980")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
